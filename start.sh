ba5dfe227c9b27e82dfaf07d186291ba5ad2e461
Nick Verbraecken <nick@verbraecken.com>
2025-02-18 17:06:51 +0100

    Always deploy the last slide

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Start the web server in the background
-npm start "theorieles 3" &
+npm start "$(ls -d theorieles* | sort -V | tail -n 1)" &
 
 # Wait for the server to be ready
 echo "Waiting for server to start..."
-while ! nc -z localhost 8000; do   
-  sleep 1
+while ! nc -z localhost 8000; do
+    sleep 1
 done
 echo "Server is ready!"
 
